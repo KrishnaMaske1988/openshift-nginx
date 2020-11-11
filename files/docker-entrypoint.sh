75cbd013450225f8c643d0e106886f2a0c869272
kmaske <krishna.maske@soprasteria.com>
2020-11-11 13:45:18 +0530

    updated

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-echo "### debug"
-echo "Run as `id`"
-ls -l /opt/activemq
-ls -l /opt/activemq
-ls -l /opt/activemq/conf
-ls -l /opt/activemq/data
-echo "###"
-
-exec "$@"
+nginx -g "daemon off;"
